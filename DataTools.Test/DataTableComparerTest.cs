3391873e950c192bbb74854b8db6e3f141fe44a2
ninerats <ninerats@gmail.com>
2016-01-10 21:38:41 -0800

    add some diff support.

---
+++ DataTools.Test/DataTableComparerTest.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTools.Test
+{
+    [TestFixture]
+    public class DataTableComparerTest
+    {
+        [Test]
+        public void Compare2DatablesTest()
+        {
+
+        }
+    }
+}
---
+++ DataTools.Test/TestHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTools.Test
+{
+    class TestHelper
+    {
+        public static DataTable GetDataTable1()
+        {
+            var dt = GetBlankDataTable();
+            dt.Rows.Add();
+            return dt;
+        }
+
+        private static DataTable GetBlankDataTable()
+        {
+            var dt = new DataTable("DataTable1");
+            var pk = new DataColumn("pk", typeof(string));
+            pk.Unique = true;
+            pk.AllowDBNull = false;
+            dt.Columns.Add(pk);
+            dt.PrimaryKey = new DataColumn[] {  pk};
+            dt.Columns.Add("varchar1", typeof(string));
+            return dt;
+        }
+    }
+}
