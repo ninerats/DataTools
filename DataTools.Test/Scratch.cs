828074a99be91a4ae6a89745f24435c1d7f2d844
ninerats <ninerats@gmail.com>
2016-01-26 22:36:16 -0800

    + scratch pad for testing random shit

---
+++ DataTools.Test/Scratch.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Craftsmaneer.DataTools.Test
+{
+    [TestFixture]
+    public class Scratch
+    {
+        [Test]
+        public void CanInnerBeNullTest()
+        {
+            var x = new Exception("text", null);
+            Assert.IsNotNull(x);
+        }
+    }
+}
